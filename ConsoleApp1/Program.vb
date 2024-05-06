1767386ab288cefee998d1d3e82436d3b36f22ec
Vladislav Němec <Nemec.Vladislav@stud.vassboskovice.cz>
2024-05-06 12:43:05 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApp1/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim èíslo As Integer
+
+        Do
+            Console.Write("zadej èíslo")
+            cislo = Console.ReadLine
+            Console.WriteLine($"Zadané èíslo je {èíslo} ")
+        Loop
+
+
+    End Sub
+End Module
+
+
+
